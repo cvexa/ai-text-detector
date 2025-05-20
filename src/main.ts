3f87758104889ef615237f041f70e8a59bc10ffc
George <cvexxxxa@abv.bg>
2025-05-20 16:55:20 +0300

    render

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,9 @@ async function bootstrap() {
   }));
   
   app.enableCors();
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 
 bootstrap(); 
